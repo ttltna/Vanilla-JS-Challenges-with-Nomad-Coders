8f3b9d3980bf7504c36c484fb0b68c3561382edc
ttltna <ttltnaaa@gmail.com>
2022-01-14 00:53:08 +0900

    feat : add function mouse enter and leave

--- HTML_in_Javascript/apps.js
+++ HTML_in_Javascript/apps.js
@@ -4,4 +4,16 @@ function handleTitleClick(){
     title.style.color= "blue";
 }
 
+function handleMouseEnter(){
+    title.style.color= "green";
+    title.innerText = "Mouse is Here!"
+}
+
+function handleMouseLeave(){
+    title.style.color= "red";
+    title.innerText = "Mouse is gone!"
+}
+
 title.addEventListener("click",handleTitleClick);
+title.addEventListener("mouseenter",handleMouseEnter);
+title.addEventListener("mouseleave",handleMouseLeave);
