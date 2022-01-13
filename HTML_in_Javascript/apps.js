44a1a3414227ef7ec6436c8c1588d0088c886305
ttltna <ttltnaaa@gmail.com>
2022-01-14 01:49:04 +0900

    style : nomad coders vanilla js challenge

--- HTML_in_Javascript/apps.js
+++ HTML_in_Javascript/apps.js
@@ -1,21 +1,5 @@
 const h1 = document.querySelector(".hello h1");
 
-// function handleTitleClick(){
-//     const currentColor = h1.style.color;
-//     let newColor;
-//     // console.log(h1.style.color);
-//     // h1.style.color = "blue";
-//     // console.log(h1.style.color);
-//     if(currentColor === "tomato"){
-//         newColor = "blue";
-//     }else if(currentColor === "blue"){
-//         newColor = "tomato";
-//     }else{
-//         newColor = "blue";
-//     }
-//     h1.style.color = newColor;
-// }
-
 function handleMouseEnter(){
     h1.style.color = "green";
     h1.innerText = "Mouse is Here!";
@@ -32,20 +16,11 @@ function handleConTextMenu(){
 }
 
 function handleWindowResize(){
-    // document.body.style.background = "violet";
     h1.style.color = "violet";
     h1.innerText = "You just resize!";
 }
 
-// h1.addEventListener("click",handleTitleClick);
-// h1.onclick = handleTitleClick;
-
-// h1.addEventListener("mouseenter",handleMouseEnter);
 h1.onmouseenter = handleMouseEnter;
-// h1.addEventListener("mouseleave",handleMouseLeave);
 h1.onmouseleave = handleMouseLeave;
-
-// h1.oncontextmenu = handleConTextMenu;
 window.addEventListener("contextmenu",handleConTextMenu)
-
 window.addEventListener("resize", handleWindowResize);
