561590403acc8d087e45c7c30778926b212dd5ce
ttltna <ttltnaaa@gmail.com>
2022-01-14 00:42:23 +0900

    feat : click addEventListener

--- HTML_in_Javascript/apps.js
+++ HTML_in_Javascript/apps.js
@@ -1,6 +1,7 @@
-const title = document.getElementById("title");
+const title = document.querySelector(".hello h1");
 
-title.innerText = "Got you!";
+function handleTitleClick(){
+    title.style.color= "blue";
+}
 
-console.log(title.id);
-console.log(title.className);
+title.addEventListener("click",handleTitleClick);
