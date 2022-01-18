3c46b9b082435fb86453c327c7c21f202c5345de
ttltna <ttltnaaa@gmail.com>
2022-01-19 02:52:56 +0900

    feat : random_number_game complete

--- Random_Number_Game/RandomNumber.js
+++ Random_Number_Game/RandomNumber.js
@@ -1,44 +1,31 @@
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
-const loginForm = document.querySelector("#login-form");
-const greetings = document.querySelector("#greeting");
+const Form = document.querySelector("#input_values");
+const randomInput = document.querySelector("#random_number input");
+const userInput = document.querySelector("#user_number input");
+const playButton = document.querySelector("#user_number button");
+const result = document.querySelector("#result");
+const chose = document.querySelector("#result #chose");
+const wonLost = document.querySelector("#result #won_lost");
 
-const HIDDEN_CLASSNAME = "hidden"; // string만 포함된 변수는 대문자로 표기하는 컨벤션
-const USERNAME_KEY = "username"
 
-function onLoginSubmit(event){
-    // console.dir(loginInput);
-    // console.log(loginInput.value);
-    // const userName = loginInput.value;
-    // if(userName === ""){
-    //     alert("Please write your name");
-    // }else if(userName.length > 15){
-    //     alert("Your name is too long");
-    // }else{
-    //     console.log(userName);
-    // }
+function handleSubmitEvent(event){
     event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const userName = loginInput.value;
-    localStorage.setItem(USERNAME_KEY,userName);
-    paintGreeting(userName);
+    const machineNum = Math.round(Math.random()*randomInput.value)
+    localStorage.setItem("random",machineNum);
+    localStorage.setItem("user",userInput.value);
+
+    if(getRandNum !== null && getUserNum !== null){
+        result.classList.remove("hidden");
+    }
+    chose.innerText = `You chose: ${userInput.value}, the machine chose: ${machineNum}`;
+    if(machineNum == userInput.value){
+        wonLost.innerText = `You won!`;
+        alert("You won!");
+    }else{
+        wonLost.innerText = `You lost!`;
+    }
 }
 
-function paintGreeting(username){
-    greetings.innerText = `Hello ${username}`;
-    greetings.classList.remove(HIDDEN_CLASSNAME);
-}
+Form.addEventListener("submit",handleSubmitEvent);
 
-// loginButton.addEventListener("click",handleLoginBtnClick);
-loginForm.addEventListener("submit",onLoginSubmit);
-
-const savedUserName = localStorage.getItem(USERNAME_KEY);
-
-if(savedUserName === null){
-    //show the form
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit",onLoginSubmit);
-}else{
-    //show the greetings
-    paintGreeting(savedUserName);
-}
+const getRandNum = localStorage.getItem("random");
+const getUserNum = localStorage.getItem("user");
