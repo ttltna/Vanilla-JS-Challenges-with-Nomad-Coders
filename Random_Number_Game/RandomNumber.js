ae5825d13e424d8e2c38e379da8f5722469cba48
ttltna <ttltnaaa@gmail.com>
2022-01-18 21:35:59 +0900

    feat : login submit preventDefault and string value is touppercase

--- Random_Number_Game/RandomNumber.js
+++ Random_Number_Game/RandomNumber.js
@@ -1,7 +1,11 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
+const loginForm = document.querySelector("#login-form");
+const loginUserName = document.querySelector("#greeting");
 
-function handleLoginBtnClick(){
+const HIDDEN_CLASSNAME = "hidden"; // string만 포함된 변수는 대문자로 표기하는 컨벤션
+
+function onLoginSubmit(event){
     // console.dir(loginInput);
     // console.log(loginInput.value);
     // const userName = loginInput.value;
@@ -12,8 +16,13 @@ function handleLoginBtnClick(){
     // }else{
     //     console.log(userName);
     // }
+    event.preventDefault();
+    loginForm.classList.add("hidden");
     const userName = loginInput.value;
     console.log(userName);
+    loginUserName.classList.remove("hidden");
+    loginUserName.innerText = "Hello " + userName;
 }
 
-loginButton.addEventListener("click",handleLoginBtnClick);
+// loginButton.addEventListener("click",handleLoginBtnClick);
+loginForm.addEventListener("submit",onLoginSubmit)
