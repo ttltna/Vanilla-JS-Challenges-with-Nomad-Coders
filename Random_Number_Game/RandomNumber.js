48f1e521bd6ed684cccb3e995a4bb69234749b40
ttltna <ttltnaaa@gmail.com>
2022-01-18 22:12:10 +0900

    feat : How to use localStorage.setItem and getItem

--- Random_Number_Game/RandomNumber.js
+++ Random_Number_Game/RandomNumber.js
@@ -1,9 +1,10 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 const loginForm = document.querySelector("#login-form");
-const loginUserName = document.querySelector("#greeting");
+const greetings = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden"; // string만 포함된 변수는 대문자로 표기하는 컨벤션
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event){
     // console.dir(loginInput);
@@ -19,9 +20,25 @@ function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const userName = loginInput.value;
-    loginUserName.classList.remove(HIDDEN_CLASSNAME);
-    loginUserName.innerText = `Hello ${userName}`;
+    localStorage.setItem(USERNAME_KEY,userName);
+    paintGreeting(userName);
+}
+
+function paintGreeting(username){
+    greetings.innerText = `Hello ${username}`;
+    greetings.classList.remove(HIDDEN_CLASSNAME);
 }
 
 // loginButton.addEventListener("click",handleLoginBtnClick);
-loginForm.addEventListener("submit",onLoginSubmit)
+loginForm.addEventListener("submit",onLoginSubmit);
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if(savedUserName === null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+    //show the greetings
+    paintGreeting(savedUserName);
+}
