0c6b7d8147847982c965b5cf123bb97e658ac81e
ttltna <ttltnaaa@gmail.com>
2022-01-20 00:43:50 +0900

    feat : setInterval and setTimeout function for clock

---
+++ Clock/clock.js
@@ -0,0 +1,11 @@
+const clock = document.querySelector("#clock")
+
+function getClock(){
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
+
+setTimeout(getClock, 5000);
