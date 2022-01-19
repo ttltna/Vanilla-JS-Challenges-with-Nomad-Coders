3b212c9e6cdfaff95f631efd5c5d8855c4ee5fcd
ttltna <ttltnaaa@gmail.com>
2022-01-20 01:54:11 +0900

    style : innerText :

--- Clock/clock.js
+++ Clock/clock.js
@@ -2,7 +2,7 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const fromNow = new Date();
-    const nowTime = fromNow.getTime();
+    let nowTime = fromNow.getTime();
     // const nowDay = fromNow.getDay();
     // const nowHours = fromNow.getHours();
     // const nowMinutes = fromNow.getMinutes();
@@ -12,7 +12,7 @@ function getClock(){
     // const nowSeconds = String(fromNow.getSeconds()).padStart(2,"0");
     
     const christmas = new Date("December 25, 2022 00:00:00");
-    const christmasTime = christmas.getTime();
+    let christmasTime = christmas.getTime();
     // const chrismasDay = christmas.getDay();
     // const chrismasHours = christmas.getHours();
     // const chrismasMinutes = christmas.getMinutes();
@@ -20,17 +20,21 @@ function getClock(){
     // clock.innerText = `${hours}:${minutes}:${seconds}`;
     // console.log(`${(chrismasDay-nowDay)}`);
 
-    const dday_ms = christmasTime - nowTime;
+    let dday_ms = christmasTime - nowTime;
 
     let dday = Math.ceil(dday_ms / (1000 * 60 * 60 * 24));
     let dhour = Math.ceil((dday_ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let dmin = Math.ceil((dday_ms % (1000 * 60 * 60)) / (1000 * 60));
     let dsec = Math.ceil((dday_ms % (1000 * 60)) / 1000);
 
+    dhour = String(dhour).padStart(2, "0");
+    dmin = String(dmin).padStart(2, "0");
+    dsec = String(dsec).padStart(2, "0");
+
     // console.log(`nowTime = ${nowTime}, christmasTime = ${christmasTime}`);
     // console.log(`남은 시간 : ${dday_ms}`);
     // console.log("D-DAY까지 " + dday + "일 " + dhour + "시간 " + dmin + "분 " + dsec + "초 남았습니다.");
-    clock.innerText = `${dday}d ${String(dhour).padStart(2,"0")}h ${String(dmin).padStart(2,"0")}m ${String(dsec).padStart(2,"0")}s`;
+    clock.innerText = `${dday}d ${dhour}h ${dmin}m ${dsec}s`;
 }
 
 getClock();
