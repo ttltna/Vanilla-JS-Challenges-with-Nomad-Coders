84a503a55a1fe8363ded35120476bf9c13f7224f
ttltna <ttltnaaa@gmail.com>
2022-01-24 01:43:56 +0900

    feat : 경도와 위도로 현재위치의 날씨,온도,지명을 브라우저에 출력하기

--- weather/weather.js
+++ weather/weather.js
@@ -6,7 +6,13 @@ function onGeoOk(position){
     console.log("You live in", lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     console.log(url);
-    fetch(url);
+    fetch(url).then((response) => response.json()).then((data) => {
+        // console.log(data.name, data.weather[0].main);
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError(){
     alert("Can't find you. No weather for you.")
