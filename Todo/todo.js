2a39ce9eaeaa4e42264857d141518a47dbb0e334
ttltna <ttltnaaa@gmail.com>
2022-01-23 14:10:12 +0900

    feat : add <li>tag child <button>tag with <li>tag remove for add To Do List third step

--- Todo/todo.js
+++ Todo/todo.js
@@ -3,11 +3,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input"); //way 2
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
-    const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click",deleteToDo);
+    const li = document.createElement("li");
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
@@ -16,6 +25,7 @@ function handleToDoSubmit(event){
     const newTodo = toDoInput.value;
     toDoInput.value = "";
     paintToDo(newTodo);
+
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
