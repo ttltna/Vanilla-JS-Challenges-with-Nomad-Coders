07fcfc3858d6381090fe571c19138456859bf00b
ttltna <ttltnaaa@gmail.com>
2022-01-23 14:19:45 +0900

    feat : add deleteToDo function with use JSON.stringify

--- Todo/todo.js
+++ Todo/todo.js
@@ -3,6 +3,13 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input"); //way 2
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos(){
+    // localStorage.setItem("todos",toDos);
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
@@ -24,7 +31,9 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 
 }
 
