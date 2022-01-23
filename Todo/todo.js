8cba6ba494f67a1b39feae2eee37ff2f19aadd32
ttltna <ttltnaaa@gmail.com>
2022-01-23 13:46:48 +0900

    feat : add To Do List first step

--- Todo/todo.js
+++ Todo/todo.js
@@ -1,8 +1,12 @@
 const toDoForm = document.getElementById("todo-form");
+// const toDoInput = document.querySelector("#todo-form input"); //way 1
+const toDoInput = toDoForm.querySelector("input"); //way 2
 const toDoList = document.getElementById("todo-list");
 
 function handleToDoSubmit(event){
     event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
